86fe26047a86e909c50d8c4b9e95593a657d3dc9
Rhys Adams <rhys@therhys.co.uk>
2024-02-12 10:08:54 +0000

    Update SSL metadata.

--- genssl.sh
+++ genssl.sh
@@ -4,7 +4,7 @@ OUT_FOLDER=.
 
 function gen {
     # hostname, out_name
-    openssl req -x509 -newkey rsa:4096 -keyout "$OUT_FOLDER/$2.key" -out "$OUT_FOLDER/$2.pem" -sha256 -days 3650 -nodes -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=$1"
+    openssl req -x509 -newkey rsa:4096 -keyout "$OUT_FOLDER/$2.key" -out "$OUT_FOLDER/$2.pem" -sha256 -days 3650 -nodes -subj "/C=GB/ST=West Midlands/L=Coventry/O=St Johns Hospital/OU=Meditech Solutions/CN=$1"
 }
 
 # DOMAINS=('sso.stjohns.local') 
