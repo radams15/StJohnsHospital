4d3bc99888e4ed137c13fbe1bb663666d92cdeb3
Rhys Adams <rhys@therhys.co.uk>
2024-02-12 11:01:24 +0000

    Better secret handling again.

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 PROJECTS=(sso FinCare CareConnect MediCloud MedRecords Prescriptions)
 # PROJECTS=(sso) 
 
-SECRET='a-very-long-secret-pls-dont-steal'
-# SECRET='the-wrong-secret' 
+# secret='a-very-long-secret-pls-dont-steal' 
+read secret
 
 TERM=xterm
 
 for proj in ${PROJECTS[@]}
 do
-    ($TERM -T "$proj" -e "bash run_single.sh $proj '$SECRET'") &
+    ($TERM -T "$proj" -e "bash run_single.sh $proj '$secret'") &
 done
