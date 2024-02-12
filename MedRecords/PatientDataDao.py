d827ebab659ca5e51ba5092ba2a13906642a9bc2
Rhys Adams <rhys@therhys.co.uk>
2024-02-12 10:45:35 +0000

    MedRecords now complies with spec.

--- MedRecords/PatientDataDao.py
+++ MedRecords/PatientDataDao.py
@@ -5,4 +5,4 @@ from EncryptedJsonFile import EncryptedJsonFile
 
 class PatientDataDao(EncryptedJsonFile):
     def __init__(self, file, key):
-        super().__init__(file, key, encrypted=True)
+        super().__init__(file, key, encrypted=False)
