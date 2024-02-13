71138c2d4ed4b74772de0d22acec0dfe72e36dd5
Rhys Adams <rhys@therhys.co.uk>
2024-02-13 12:19:44 +0000

    Move gen_otp to root, convert shell scripts to Python to allow running on Windows.

--- gen_otp.py
+++ gen_otp.py
@@ -1,14 +1,16 @@
-from sys import argv
+from sys import argv, path
 
 import pyotp
 
-from UserDao import UserDao
+path.append('./shared')
+from sso.UserDao import UserDao
+
 
 if len(argv) != 3:
     print(f'Usage: {argv[0]} SECRET USERNAME')
     exit(1)
 
-user_dao = UserDao('users.json', argv[1])
+user_dao = UserDao('sso/users.json', argv[1])
 
 user = user_dao.get_user(argv[2])
 
---
+++ run.py
@@ -0,0 +1,40 @@
+import subprocess
+import atexit
+import os
+
+from sys import argv
+from os.path import abspath
+
+CERT = abspath('./localhost.pem')
+KEY = abspath('./localhost.key')
+
+PROJECTS = {
+        'sso': 1111,
+        'FinCare': 3333,
+        'CareConnect': 3355,
+        'MediCloud': 5555,
+        'MedRecords': 2222,
+        'Prescriptions': 4444
+}
+
+env = os.environ.copy()
+
+def run_single(name, host, secret):
+    port = PROJECTS[name]
+    env['SECRET'] = secret
+    return subprocess.Popen(['flask', 'run', f'--host={host}', f'--port={port}', f'--cert={CERT}', f'--key={KEY}'], cwd=name, env=env)
+
+processes = []
+secret = 'a-very-long-secret-pls-dont-steal'
+
+for project in PROJECTS.keys():
+    processes.append(run_single(project, 'localhost', secret))
+
+def on_exit():
+    print('Terminating Processes')
+    for p in processes:
+        p.terminate()
+
+atexit.register(on_exit)
+
+input('Press any key to exit...')
